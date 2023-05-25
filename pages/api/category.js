5ee9c93b653a33f104a2bbb8e30fdd5bc9ed3058
ikamrava <kamrava.iman@outlook.com>
2023-05-25 22:09:39 +0100

    properties

--- pages/api/category.js
+++ pages/api/category.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 
 export default async function handler(req, res) {
     const {method } =req;
-    const {category} = req.body;
+    const {category,properties} = req.body;
     console.log(req.body)
     
     await mongoose.connect(process.env.MONGODB_URI)
@@ -13,17 +13,17 @@ export default async function handler(req, res) {
         category: {
             type: String,
             required: true,
-          }
+          },
+          properties: [{
+            type: Object,
+            required: false,
+          }],
       });
 
     const Category = mongoose.models.Category || mongoose.model('Category', schema);
-      
-      
 
     if(method === 'POST'){
-        
-        await Category.create({ category});
-        
+        await Category.create({category,properties:properties});
         res.status(200).json("The category has been added")
 
    }if(method === 'GET'){
@@ -37,6 +37,9 @@ export default async function handler(req, res) {
        const {id} = req.query;
        res.status(200).json(await Category.deleteOne({_id:id}))
 
+   }if(method === 'PUT'){
+       const {id} = req.query;
+       res.status(200).json(await Category.updateOne({_id:id}, {category,properties:properties}))
    }
 }
 
--- pages/categories.js
+++ pages/categories.js
@@ -3,11 +3,36 @@ import Layout from '../components/Layout'
 import axios from 'axios';
 import Swal from 'sweetalert2'
 import 'sweetalert2/dist/sweetalert2.css';
+import { set } from 'mongoose';
 
 
 function category() {
   const [category, setCategory] = useState('');
   const [categories, setCategories] = useState([]);
+  const [properies, setProperies] = useState([]);
+  const [editedcat,setEditedCat] = useState(null)
+
+  function propertyHandler(){
+    setProperies(prev=>{
+      return [...prev, {name:'', value:''}]
+    })
+  }
+
+  function handleChangePropertyName(item,newName,index){
+     setProperies(prev=>{
+      const newArr = [...prev]
+      newArr[index].name = newName
+      return newArr
+     })
+  }
+
+ function handleChangePropertyValue(item,newValue,index){
+  setProperies(prev=>{
+   const newArr = [...prev]
+   newArr[index].value = newValue
+   return newArr
+  })
+}
 
 
 const  showConfirmation = (_id) => {
@@ -20,8 +45,6 @@ const  showConfirmation = (_id) => {
     showCancelButton: true,
     confirmButtonText: 'Yes, delete it!',
     cancelButtonText: 'No, cancel!',
-
-
 
   }).then((result) => {
     if (result.isConfirmed) {
@@ -40,6 +63,7 @@ const  showConfirmation = (_id) => {
   async function fetchData(){
     const res = await axios.get('http://localhost:3000/api/category')
     setCategories(res.data)
+    
   }
 
   useEffect(()=>{
@@ -47,62 +71,122 @@ const  showConfirmation = (_id) => {
   },[])
 
   async function addHandler(){
+    const propertiesObject = properies.map(item=>{
+      return {name:item.name, value:item.value.split(",")}
+    })
       if(!category) {
         alert('Category is required')
         return
       }
-    
-    
-      if(categories.some((item)=>item.category === category)){
+
+      if(categories.some((item)=>item.category === category)&& (!editedcat)){
         alert('Category already exists')
         return
+      }
+      
+      if(editedcat){
+        const res = await axios.put(`http://localhost:3000/api/category?id=${editedcat._id}`,{category,properties:propertiesObject})
+        fetchData()
+        setEditedCat(null)
+        setCategory('')
+        setProperies([])
+        
+        return
       }else{
-      const cat = await axios.post('http://localhost:3000/api/category', {category})
-      fetchData()
-      setCategory('')
-    }
-  }
-  
-
-
-
+        const cat = await axios.post('http://localhost:3000/api/category', {category,properties:propertiesObject})
+        fetchData()
+        setCategory('')
+        setProperies([])
+      
+      }
+      
     
+  }
+
+  function updateHandler(category){
+    setEditedCat(category)
+    const cat = categories.find((item)=>item._id === category._id)
+    setCategory(cat.category)
+
+    console.log(cat.properties)
+    if(cat.properties.length > 0){
+      cat.properties.map(item=>{
+      const name = item.name
+      const value = item.value.join()
+      setProperies(prev=>{
+        return [...prev,{name,value}]
+      })
+    })
+  }
+    
+    // setProperies(cat.properties.map(item=>{
+    //   return {name:item.name, value:item.value.split(",")}
+    
+  }
+
+
   
-
-
-
-
-
   return (
     <Layout>
-        <label className=' mb-2 font-bold'>New Category Name</label>
+        <label className=' mb-2 font-bold'>{editedcat? `Update ${editedcat.category} Category`:'Add Category'}</label>
        <input value={category} onChange={(e)=>setCategory(e.target.value)}  name="category" id="category" className='input input-bordered w-full max-w-xs border-2 p-1' type="text" placeholder='Category Name'/>
-       <button onClick={addHandler} className='btn btn-primary px-4 py-2 rounded-lg text-white font-bold text-center bg-slate-700 mt-3 w-36'>Add Category</button>
+       
+       
+       <div className='flex  mt-5'>
+        <button onClick={propertyHandler} className='btn btn-primary px-3  py-2 rounded-lg text-white font-bold text-center bg-blue-700  '>Add Property</button>
+       </div>
+       <div className='ml-2'>
+          {properies.length > 0 && properies.map((item,index)=>(
+            <div className='flex gap-2 mt-3 '>
+              <input value={item.name}
+               onChange={(e)=>handleChangePropertyName(item,e.target.value,index)}
+               className='input input-bordered  border-2 px-2 py-1 rounded-lg font-bold' type="text" placeholder='Property Name'/>
+              <input value={item.value}
+              onChange={(e)=>handleChangePropertyValue(item,e.target.value,index)}
+               className='input input-bordered  border-2 px-2 py-1 rounded-lg font-bold' type="text" placeholder='Property Value'/>
+             <button className='btn btn-primary px-3  py-2 rounded-lg text-white font-bold text-center bg-red-700 ' onClick={()=>setProperies(prev=>prev.filter((item,ind)=>ind!==index))}>Delete</button>
+            </div>
+            ))}
+       </div>
+
+       <div className='flex gap-3 '>
+       <button onClick={addHandler} className='btn btn-primary px-4 py-2 rounded-lg text-white font-bold text-center bg-slate-700 mt-3 '>Save</button>
+       <button onClick={()=>{
+        setEditedCat(null)
+        setCategory('')
+        setProperies([])}} 
+        className='btn btn-primary px-4 py-2 rounded-lg text-white font-bold text-center bg-gray-500 mt-3 '>Cancel</button>
+       
+       </div>
+       
+       
+       
        <div className='flex flex-col'> 
 
-       <table className='table-auto  mt-8 max-w-md '>
+       {!editedcat && (
+         <table className='table-auto  mt-8 max-w-md '>
 
-       <thead >
-              <tr className='bg-slate-700 text-white text-left  '>
-                <th className='p-1 border-2 border-black'>Category Name</th>
-                <th className='p-1 border-2 border-black text-center'>Action</th>
-              </tr>
-              </thead>
-        { categories.length > 0  && categories.map((item)=>(
-          
-         
-            
-              <tbody>
-                <tr>
-                  <td className='p-1 border-2 border-black' >{item.category}</td>
-                  <td className='p-1 border-2 border-black text-red-700 text-center cursor-pointer' onClick={()=>showConfirmation(item._id)}>Delete</td>
-                  </tr>
-              </tbody>
-      
+         <thead >
+                <tr className='bg-slate-700 text-white text-left w-full  '>
+                  <th className='p-1 border-2 border-black w-full'>Category Name</th>
+                  <th className='p-1 border-t-2 border-b-2 border-black '></th>
+                  <th className='p-1 border-t-2 border-b-2 border-black border-r-2'></th>
+                 
+                </tr>
+                </thead>
+          { categories.length > 0  && categories.map((item)=>(
+                <tbody>
+                  <tr>
+                    <td className='p-1 border-2 border-black' >{item.category}</td>
+                    <td className='p-1 border-2 border-black text-center cursor-pointer' onClick={()=>updateHandler(item)}>Edit</td>
+                    <td className='p-1 border-2 border-black text-red-700 text-center cursor-pointer' onClick={()=>showConfirmation(item._id)}>Delete</td>
+                    </tr>
+                </tbody>
+              ))}
+                </table>
+       )}
 
       
-            ))}
-              </table>
        </div>
     </Layout>
   )
